60f8ab3d7372c96122a6a9871e9b62a02ff13df7
Christian Jarboe <Christiandjarboe@gmail.com>
2018-11-30 15:32:29 -0600

    Init commit

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,46 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+
+  var intturn = Math.random()*2;
+  var turn;
+  var turncount=0;
+  var board =[null,null,null,
+              null,null,null,
+              null,null,null];
+
+  if(intturn>1){
+    turn="player1";
+  } else{turn="player2";}
+  
+  $('[data-cell]').on('click', function(){
+
+      if($(this).text() ===''){
+          if(turn ==="player1"){
+            $(this).text("x");
+            board[$(this).data().cell]="x";
+            turn="player2";
+          }else{
+            $(this).text("x");
+            board[$(this).data().cell]="x";
+            turn="player1";
+        }
+        turncount++;
+        checkWin();
+        console.log(turncount);
+        
+      }
+
+  })
+
+  function checkWin(){
+    for(let i=0;i<9;i++){ 
+     
+      }
+    
+        
+  }
+  
+
+
 });
