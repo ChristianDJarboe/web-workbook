9503a2f2f65bfd66e5ebbfbf2dbf21ef0b6aa17c
Christian Jarboe <ChristianDJarboe@gmail.com>
2018-12-05 11:09:49 -0600

    Final - Two player tic tac toe game, no AI implementation. There is a bug where the last play is not displayed on the screen before starting the next game. Not sure why this is because the display is updated before the checkwin function call.

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -2,7 +2,7 @@
 var intturn = Math.random()*2;
 var turn;
 var turncount=0;
-var reset = false;
+var win = false;
 var board =[null,null,null,
             null,null,null,
             null,null,null];
@@ -17,8 +17,8 @@ $(document).ready(function() {
   } else{turn="player2";}
   //Click function, changes ever turn.
   $('[data-cell]').on('click', function(){
-
-      if(board[$(this).data().cell]=''){   //Fills the array board
+   
+      if(board[$(this).data().cell]===null){   //Fills the array board
           if(turn ==="player1"){
             board[$(this).data().cell]="X";
             turn="player2";
@@ -27,37 +27,77 @@ $(document).ready(function() {
             turn="player1";
         }
         //Prints board
+        /*This should print and be visible before the checkwin function
+        is called but for some reason the final play is never displayed
+        on the board. Very confusing. */
         $(this).text(board[$(this).data().cell]);
-        //Clears board
-        if(reset ===true){
-          for(let i=0;i<9;i++){
-            $(this).text='a';
-
-          }
-        }
-        turncount++;
-        console.log(turncount);
-      
+        checkWin(turn);
       }
-
+    
   })
 //-----------------------------------------
   $('#clear').on("click", function(){
-    console.log("ASSS");
+    clear();
+  })
+  //Clears the board and resets turn count
+  function clear(){
+    console.log("Clearing");
+    var cell = document.getElementsByClassName("cell");
+
     for(let i=0;i<9;i++){
       board[i]=null;
-  
+      cell[i].innerHTML="";
      
     }
-    reset = true;
     turncount=0;
-  
-  })
+  }
 
 
-
-
-
+  function checkWin(turn){
+    var x;
+    if(turn === "player2"){x="X"}
+    else{x="O"}
+    //Checks for horizontal wins
+    if(board[0]===x && board[1]===x && board[2]===x){
+      console.log("Top row win");
+      win=true;
+    }
+    if(board[3]===x && board[4]===x && board[5]===x){
+      console.log("Middle row win");
+      win=true;
+    }
+    if(board[6]===x && board[7]===x && board[8]===x){
+      console.log("Bottom row win");
+      win=true;
+    }
+    //Checks for vertical wins
+    if(board[0]===x && board[3]===x && board[6]===x){
+      console.log("Left column win");
+      win=true;
+    }
+    if(board[1]===x && board[4]===x && board[7]===x){
+      console.log("Middle column win");
+      win=true;
+    }
+    if(board[2]===x && board[5]===x && board[8]===x){
+      console.log("Right column win");
+      win=true;
+    }
+    //Checks for diagonal wins
+    if(board[0]===x && board[4]===x && board[8]===x){
+      console.log("Backslash win");
+      win=true;
+    }
+    if(board[2]===x && board[4]===x && board[6]===x){
+      console.log("Forwardslash win");
+      win=true;
+    }
+    if(win ===true){
+      alert(turn+" won!");
+      clear();
+      win = false;
+    }
+  }
 });
 
 
